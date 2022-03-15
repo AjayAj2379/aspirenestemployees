0805c81409adb735cf373103cb57ef277152515c
eswaribala <parameswaribala@gmail.com>
2022-03-15 18:14:28 +0530

    updated

--- src/models/taskdto.ts
+++ src/models/taskdto.ts
@@ -1,5 +1,10 @@
+import {ApiProperty} from "@nestjs/swagger";
+
 export class TaskDTO{
+    @ApiProperty()
     taskId:number;
+    @ApiProperty()
     taskName:string;
+    @ApiProperty()
     deadline:Date;
 }
