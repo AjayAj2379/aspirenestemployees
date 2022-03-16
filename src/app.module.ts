d507c9591f0edebd616d7a4143792dacdee85b39
eswaribala <parameswaribala@gmail.com>
2022-03-16 18:29:14 +0530

    updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,11 @@ import {MongooseModule} from "@nestjs/mongoose";
 
 import { TaskModule } from './task/task.module';
 import { EmployeeModule } from './employee/employee.module';
+import { GraphqlModule } from './graphql/graphql.module';
 
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/employeemgmtdb'),TaskModule, EmployeeModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/employeemgmtdb'),TaskModule, EmployeeModule, GraphqlModule],
   controllers: [AppController],
   providers: [AppService],
 })
